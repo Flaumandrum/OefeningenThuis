c2c13a71be817ff1b9e7de6f248fcbfaefaa9287
tomad <tomad@LAPTOP-9Q32PNAB>
2022-12-27 11:50:51 +0100

    Add project files.

---
+++ OefeningenThuis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OefeningenThuis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sla in deze repository (niet in dit document) de oefeningen op die je thuismaakt.
+        }
+    }
+}
